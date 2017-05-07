3decb386853ccf92887c00940e86445af50c3727
Jose Valdebenito <pvpj017@gmail.com>
2017-05-07 15:48:38 -0300

    Modified: Added user views and yields

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+        <!-- meta tributo title -->
         <title>@yield('title')</title>
+
+        <!-- Estilos de la vista -->
         @yield('styles')
     </head>
     <body @yield('body-attr')>
+        <!-- Este yield tiene relacion al contenido de la pagina -->
         @yield('content')
+
+        <!-- Con este yield se inyectan los scripts -->
+        @yield('scripts')
     </body>
 </html>
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,13 +1,55 @@
+
+<!-- Extiende el master layout -->
 @extends('layouts.master')
 
+<!-- meta atributo title -->
 @section('title')
     Login - OID
 @endsection
 
+<!-- inyeccion de estilos -->
+@section('styles')
+    <title>Sign in | Flatty - Flat Administration Template</title>
+
+    <link href="/assets/stylesheets/bootstrap/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <!-- / theme file [required] -->
+    <link href="/assets/stylesheets/light-theme.css" rel="stylesheet" type="text/css" media="all" id="color-settings-body-color" />
+    <!-- / coloring file [optional] (if you are going to use custom contrast color) -->
+    <link href="/assets/stylesheets/theme-colors.css" rel="stylesheet" type="text/css" media="all" />
+@endsection
+
+<!-- Atributos del body -->
 @section('body-attr')
     class='contrast-red login contrast-background'
 @endsection
 
+<!-- Inyeccion de scripts
+     No importa que vayan antes del body, en el master layout se estan insertando alfinal.
+-->
+@section('scripts')
+    <!-- / jquery [required] -->
+    <script src="assets/javascripts/jquery/jquery.min.js" type="text/javascript"></script>
+    <!-- / jquery mobile (for touch events) -->
+    <script src="assets/javascripts/jquery/jquery.mobile.custom.min.js" type="text/javascript"></script>
+    <!-- / jquery ui -->
+    <script src="assets/javascripts/jquery/jquery-ui.min.js" type="text/javascript"></script>
+    <!-- / jQuery UI Touch Punch -->
+    <script src="assets/javascripts/jquery/jquery.ui.touch-punch.min.js" type="text/javascript"></script>
+    <!-- / bootstrap [required] -->
+    <script src="assets/javascripts/bootstrap/bootstrap.js" type="text/javascript"></script>
+    <!-- / modernizr -->
+    <script src="assets/javascripts/plugins/modernizr/modernizr.min.js" type="text/javascript"></script>
+    <!-- / retina -->
+    <script src="assets/javascripts/plugins/retina/retina.js" type="text/javascript"></script>
+    <!-- / theme file [required] -->
+    <script src="assets/javascripts/theme.js" type="text/javascript"></script>
+    <!-- / START - page related files and scripts [optional] -->
+    <script src="assets/javascripts/plugins/validate/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="assets/javascripts/plugins/validate/additional-methods.js" type="text/javascript"></script>
+    <!-- / END - page related files and scripts [optional] -->
+@endsection
+
+<!-- Contenido del body -->
 @section('content')
       <div class='middle-container'>
         <div class='middle-row'>
