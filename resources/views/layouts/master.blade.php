53376bad74f07cb44ac60a13ce1318ebce9f7a4e
Jose Valdebenito <pvpj017@gmail.com>
2017-05-07 18:56:48 -0300

    Modified: refactoring neccesary styles to master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,23 @@
         <meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
+        <!-- / jquery [required] -->
+        <script src="assets/javascripts/jquery/jquery.min.js" type="text/javascript"></script>
+        <!-- / jquery mobile (for touch events) -->
+        <script src="assets/javascripts/jquery/jquery.mobile.custom.min.js" type="text/javascript"></script>
+        <!-- / jquery ui -->
+        <script src="assets/javascripts/jquery/jquery-ui.min.js" type="text/javascript"></script>
+        <!-- / jQuery UI Touch Punch -->
+        <script src="assets/javascripts/jquery/jquery.ui.touch-punch.min.js" type="text/javascript"></script>
+        <!-- / bootstrap [required] -->
+        <script src="assets/javascripts/bootstrap/bootstrap.js" type="text/javascript"></script>
+        <!-- / modernizr -->
+        <script src="assets/javascripts/plugins/modernizr/modernizr.min.js" type="text/javascript"></script>
+        <!-- / retina -->
+        <script src="assets/javascripts/plugins/retina/retina.js" type="text/javascript"></script>
+        <!-- / theme file [required] -->
+        <script src="assets/javascripts/theme.js" type="text/javascript"></script>
+
         <!-- meta tributo title -->
         <title>@yield('title')</title>
 
