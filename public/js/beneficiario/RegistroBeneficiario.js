a08eb541b32f3a4dbdbc47b66358fe80c59d6b7f
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-05-09 01:55:32 -0300

    Fixed: Cambio en las referencias a .css en master

---
+++ public/js/beneficiario/RegistroBeneficiario.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Alfredo Viccenzo on 09-05-2017.
+ */
+
+$('#myWizard').on('finished.fu.wizard', function (evt, data) {
+    alert('Terminaste el formulario');
+});
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,11 @@
         <meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         @yield('styles_before')
-        <link href="/assets/stylesheets/bootstrap/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="{{ asset('/assets/stylesheets/bootstrap/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
         <!-- / theme file [required] -->
-        <link href="/assets/stylesheets/light-theme.css" rel="stylesheet" type="text/css" media="all" id="color-settings-body-color" />
+        <link href="{{ asset('/assets/stylesheets/light-theme.css') }}" rel="stylesheet" type="text/css" media="all" id="color-settings-body-color" />
         <!-- / coloring file [optional] (if you are going to use custom contrast color) -->
-        <link href="/assets/stylesheets/theme-colors.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="{{ asset('/assets/stylesheets/theme-colors.css') }}" rel="stylesheet" type="text/css" media="all" />
 
         <!-- meta tributo title -->
         <title>@yield('title')</title>
