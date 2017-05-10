ac4e0c19b9a699db686c2bd5a37c2879f31ba13c
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-05-09 21:55:13 -0300

    Modified: Vista registro de usuario validaciones

--- public/js/beneficiario/RegistroBeneficiario.js
+++ public/js/beneficiario/RegistroBeneficiario.js
@@ -2,53 +2,58 @@
  * Created by Alfredo Viccenzo on 09-05-2017.
  */
 
-var validator = $("#formulario-registro").validate({
-    rules: {
-        nombres: {
-            required: true,
-            lettersonly: true
-        },
-        apellidos: {
-            required: true,
-            lettersonly: true
-        },
-        email: {
-            required: true,
-            email: true
-        }
-    },
-    messages: {
-        nombres: {
-            required: 'Debe ingresar un nombre.',
-            lettersonly: 'Solo debe ingresar letras.'
-        },
-        apellidos: {
-            required: 'Debe ingresar ambos apellidos.',
-            lettersonly: 'Solo debe ingresar letras.'
-        },
-        email: {
-            required: 'Debe ingresar una dirección e-mail.',
-            email: 'Por favor ingrese una dirección válida.'
-        }
-    }
-});
+//var validator = $('#formulario-registro').validator();
+$('#formulario-registro').validator();
 
-$('#myWizard').on('actionclicked.fu.wizard', function (e, data) {
+$('#myWizard').wizard().on('actionclicked.fu.wizard', function (e, data) {
+
     if (data.direction === 'previous') {
         // Do nothing if you're going to the previous step
         return;
     }
 
-
-	$.validator.addMethod("buga", (function(value) {
-      return value === "buga";
-    }), "Please enter \"buga\"!");
-
-    $.validator.methods.equal = function(value, element, param) {
-      return value === param;
-    };
-
+    //e.preventDefault();
 
 }).on('finished.fu.wizard', function(e) {
-    // Mandar el formulario
+    alert('salir');
 });
+
+
+
+
+// var validator = $("#formulario-registro").validate({
+//     rules: {
+//         nombres: {
+//             required: true,
+//             lettersonly: true
+//         },
+//         apellidos: {
+//             required: true,
+//             lettersonly: true
+//         },
+//         email: {
+//             required: true,
+//             email: true
+//         },
+//         rut: {
+//             required: true
+//         }
+//     },
+//     messages: {
+//         nombres: {
+//             required: 'Debe ingresar un nombre.',
+//             lettersonly: 'Solo debe ingresar letras.'
+//         },
+//         apellidos: {
+//             required: 'Debe ingresar ambos apellidos.',
+//             lettersonly: 'Solo debe ingresar letras.'
+//         },
+//         email: {
+//             required: 'Debe ingresar una dirección e-mail.',
+//             email: 'Por favor ingrese una dirección válida.'
+//         },
+//         rut: {
+//             required: 'Debe ingresar el rut.'
+//         }
+//     }
+// });
