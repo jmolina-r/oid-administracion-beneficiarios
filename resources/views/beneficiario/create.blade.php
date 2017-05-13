6bc2a004c2db6cce909caf593d903ee27b48f8aa
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-05-13 17:42:56 -0300

    Modified: Ejemplo en rut

--- resources/views/beneficiario/create.blade.php
+++ resources/views/beneficiario/create.blade.php
@@ -150,9 +150,9 @@ class='contrast-red'
                           </div>
 
                           <div class='col-md-12 form-group'>
-                           <label class='control-label' for='inputText'>Cédula de identidad</label>
+                           <label class='control-label' for='inputText'>Cédula de identidad (Sin puntos con guión)</label>
                            <div class='controls'>
-                            <input name="rut" value="{{ old('rut') }}" class='form-control' id='inputText' placeholder='Cédula de identidad' type='text' required pattern="\d{3,8}-[\d|kK]{1}" maxlength="200">
+                            <input name="rut" value="{{ old('rut') }}" class='form-control' id='inputText' placeholder='Ej. 12345678-8' type='text' required pattern="\d{3,8}-[\d|kK]{1}" maxlength="200">
                             <div class="help-block with-errors"></div>
                           </div>
                         </div>
