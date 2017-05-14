8bf204d474c9e6041c906f62147591dedbe87ab9
Jose Valdebenito <pvpj017@gmail.com>
2017-05-13 22:33:16 -0300

    Modified: number validation

--- public/js/InputValidation.js
+++ public/js/InputValidation.js
@@ -1,16 +1,15 @@
 /**
- * Metodos adicionales para las validaciones para los input 
+ * Metodos adicionales para las validaciones para los input
  * de los formularios
 */
-//Input que contiene solo letras
-$('.onlyletters').bind('keyup blur',function(){ 
+//Input que no contiene numeros ni caracteres extraños, pero si acepta tildes
+$('.onlyletters').bind('keyup blur',function(){
     var node = $(this);
-    node.val(node.val().replace(/[^a-zA-Z ]/g,'') ); }
+    node.val(node.val().replace(/((\d)+|[‚´≠”“÷¬∞¢¿·ºª"~!@#$%^&*\(\)_+=`{}\[\]\|\\:;'<>,.\/?"\-])/g,'') ); }
 );
 
 //Input que contiene solo numeros
-$('.onlynumbers').bind('keyup blur',function(){ 
+$('.onlynumbers').bind('keyup blur',function(){
     var node = $(this);
     node.val(node.val().replace(/[^0-9 ]/g,'') ); }
 );
-
