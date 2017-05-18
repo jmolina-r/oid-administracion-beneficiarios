d8be8bb66c5761eedf6adc53dca090e3505bd29f
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-05-18 19:51:20 -0400

    Modified: Fecha de nacimiento

--- public/js/beneficiario/RegistroBeneficiario.js
+++ public/js/beneficiario/RegistroBeneficiario.js
@@ -106,10 +106,8 @@ $(document).ready(function() {
             maxDate:"now",
             format: "L",
             icons: {
-                time: "fa fa-clock-o",
-                date: "fa fa-calendar",
-                up: "fa fa-arrow-up",
-                down: "fa fa-arrow-down"
+                previous: 'fa fa-chevron-left',
+                next: 'fa fa-chevron-right'
             },
             viewMode: 'years'
         });
