9bb4db04a1250816d77c7ae2a2c0c24536ad3209
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-05-18 22:01:09 -0400

    Modified: Maxlength fono tutor

--- resources/views/beneficiario/create.blade.php
+++ resources/views/beneficiario/create.blade.php
@@ -328,7 +328,7 @@ class='contrast-red'
                         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                           <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 form-group input-group'>
                             <span class='capitalize input-group-addon'>+56</span>
-                            <input name='telefono_tutor' value="{{ old('telefono_tutor') }}" class='form-control onlynumbers' id='inputText' placeholder='Teléfono de contacto' type='text'>
+                            <input name='telefono_tutor' value="{{ old('telefono_tutor') }}" class='form-control onlynumbers' id='inputText' placeholder='Teléfono de contacto' type='text' maxlength="9">
                           </div>
                         </div>
                       </div>
