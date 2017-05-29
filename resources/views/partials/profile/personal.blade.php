a2f69609c5d36fe45667fc032435e0c950260d17
Jose Valdebenito <pvpj017@gmail.com>
2017-05-28 22:41:32 -0400

    Modified: profile.personal info added

--- resources/views/partials/profile/personal.blade.php
+++ resources/views/partials/profile/personal.blade.php
@@ -2,7 +2,10 @@
     <div class="row">
         <div class="col-md-12">
             <p class="capitalize"><span class="tit">Nombre Completo</span> {{$persona->nombre}} {{$persona->apellido}}</p>
+            <p class="capitalize"><span class="tit">Rut</span> {{$persona->rut}}</p>
+            <p class="capitalize"><span class="tit">Genero</span> {{$persona->sexo}}</p>
+            <p class="capitalize"><span class="tit">Fecha de Nacimiento</span> {{$persona->fecha_nacimiento}}</p>
+            <p><span class="tit">País de Origen</span> <span class="capitalize">{{$persona->pais->nombre}}</span></p>
         </div>
     </div>
 </div>
-        
