91c4acf0daa6e924cfc26178127c4dcaec26ddab
Jose Valdebenito <pvpj017@gmail.com>
2017-05-29 03:53:30 -0400

    Modified: Person profile cleaned

--- resources/views/partials/profile/personal.blade.php
+++ resources/views/partials/profile/personal.blade.php
@@ -4,7 +4,7 @@
             <p class="capitalize"><span class="tit">Nombre Completo</span> {{$persona->nombre}} {{$persona->apellido}}</p>
             <p class="capitalize"><span class="tit">Rut</span> {{$persona->rut}}</p>
             <p class="capitalize"><span class="tit">Genero</span> {{$persona->sexo}}</p>
-            <p class="capitalize"><span class="tit">Fecha de Nacimiento</span> {{$persona->fecha_nacimiento}}</p>
+            <p class="capitalize"><span class="tit">Fecha de Nacimiento</span> {{date('d/m/Y', strtotime($persona->fecha_nacimiento))}}</p>
             <p><span class="tit">País de Origen</span> <span class="capitalize">{{$persona->pais->nombre}}</span></p>
         </div>
     </div>
