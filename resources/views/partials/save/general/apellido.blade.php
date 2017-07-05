145d733d2bb9fc566882b0a4c1efc06211942773
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-07-05 03:32:56 -0400

    Modified: Componentes registro de usuario

--- resources/views/partials/save/general/apellido.blade.php
+++ resources/views/partials/save/general/apellido.blade.php
@@ -1,7 +1,15 @@
 <div class='form-group'>
     <label class='control-label' for='inputText'>Apellidos</label>
     <div class='controls'>
-        <input name='apellidos' value="{{ old('apellidos') }}" class='form-control onlyletters' id='inputText' placeholder='Apellidos' type='text' required>
+        <input name='apellidos' class='form-control onlyletters' id='inputText' placeholder='Apellidos' type='text' required value=
+        @if(old('apellidos'))
+            "{{ old('apellidos') }}"
+        @elseif(isset($persona))
+            "{{ $persona->apellido }}"
+        @else
+            ""
+        @endif
+        >
         <div class="help-block with-errors">
         </div>
     </div>
