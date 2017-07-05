8f7543e60f643004ee37287928c4c4a7951191bb
Bryan <bmirandag@hotmail.com>
2017-07-04 21:13:20 -0400

    Modifier: fichaSocial

--- app/Http/Controllers/FichaSocialController.php
+++ app/Http/Controllers/FichaSocialController.php
@@ -42,22 +42,13 @@ class FichaSocialController extends Controller
             $motivoVisita = $request -> input('vd');
             $obsVisita = $request -> input('vdText');
 
-<<<<<<< HEAD
+
             for($i=0;$i<count($motivoVisita);$i++){
 
                 $textPos = $motivoVisita[$i]-7;
                 // "Observacion: ". $obsVisita[$textPos] . "<br>";
                 if($obsVisita[$textPos]!=NULL){
                     $obsIt = $obsVisita[$textPos];
-=======
-            $this->validate($request, ['vd' => 'required',]);
-
-            for($i=0;$i<count($obsVisita);$i++){
-               
-                if($obsVisita[$i]!=null){
-           
-                    $obsIt = $obsVisita[$i];
->>>>>>> d6bc37e480eaa3a46974f29ce2c6b37f1cbe4c98
                 }
                 $motivoSocial = new \App\MotivoAtencionSocial([
 
