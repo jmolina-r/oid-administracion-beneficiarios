c258bea674f19efc46daa598c3bd01008fd8167b
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-07-04 23:24:06 -0400

    Modified: Componentes registro ingreso

--- resources/views/partials/save/general.blade.php
+++ resources/views/partials/save/general.blade.php
@@ -36,80 +36,16 @@
     </div>
 
     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <label class='control-label' for='inputSelect'>
-                Credencial de discapacidad
-            </label>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                @include('partials.save.general.credencial-discapacidad')
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                @include('partials.save.general.credencial-vencimiento')
-            </div>
-        </div>
+        @include('partials.save.general.credencial-discapacidad')
     </div>
 
     <!-- Falta aqui manejar la subida de archivo para agregar la credencial -->
 
     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <label class='control-label' for='inputSelect'>
-                Registro social de hogares
-            </label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                @include('partials.save.general.registro-social-hogares')
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div class='form-group'>
-                    <div class="input-group">
-                        <input name='registro_social_porcentaje' class='form-control' placeholder='Porcentaje' type='number' min="0" max="100" id="registro_social_porcentaje" disabled>
-                        <span class='input-group-addon'>%</span>
-                    </div>
-                    <div class="help-block with-errors">
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('partials.save.general.registro-social-hogares')
     </div>
 
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <label class='control-label' for='inputSelect'>
-                Acompañante o tutor
-            </label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class='form-group'>
-                    <input name='nombre_tutor' value="{{ old('nombre_tutor') }}" class='form-control onlyletters' id='inputText' placeholder='Nombre' type='text' required maxlength="200">
-                    <div class="help-block with-errors">
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                <div class='form-group'>
-                    <input name='apellido_tutor' value="{{ old('apellido_tutor') }}" class='form-control onlyletters' id='inputText' placeholder='Apellidos' type='text' required maxlength="200">
-                    <div class="help-block with-errors">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div class='form-group'>
-                    <div class="input-group">
-                        <span class='capitalize input-group-addon'>+56</span>
-                        <input name='telefono_tutor' value="{{ old('telefono_tutor') }}" class='form-control onlynumbers' id='inputText' placeholder='Teléfono de contacto' type='text' maxlength="9">
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('partials.save.general.tutor')
     </div>
 </div>
--- resources/views/partials/save/general/credencial-discapacidad.blade.php
+++ resources/views/partials/save/general/credencial-discapacidad.blade.php
@@ -1,7 +1,31 @@
-<div class='form-group'>
-    <select style="width:100%;" name='credencial_discapacidad' value="{{ old('credencial_discapacidad') }}" class='form-control select-tag' id='credencial_discapacidad' required>
-       <option value='0'>No</option>
-       <option value='2'>En trámite</option>
-       <option value='1'>Si</option>
-    </select>
+<div class="form-group">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <label class='control-label' for='inputSelect'>
+            Credencial de discapacidad
+        </label>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class='form-group'>
+                <select style="width:100%;" name='credencial_discapacidad' value="{{ old('credencial_discapacidad') }}" class='form-control select-tag' id='credencial_discapacidad' required>
+                   <option value='0'>No</option>
+                   <option value='2'>En trámite</option>
+                   <option value='1'>Si</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="form-group">
+                <div class='input-group' id='credencial_venc'>
+                    <input name='credencial_vencimiento' class='form-control' data-format='DD/MM/YYYY' placeholder='Vencimiento' type='text' id="credencial_vencimiento" disabled>
+                    <span class='input-group-addon'>
+                        <span class='fa fa-calendar'></span>
+                    </span>
+                </div>
+                <div class="help-block with-errors">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
