45f44ca2371da7e3910949fdb9c6ab5cdc234aa4
Jose Valdebenito <pvpj017@gmail.com>
2017-07-06 02:49:10 +0200

    Modified: getting information about Organizacion on Beneficiario register

--- resources/views/partials/profile/social.blade.php
+++ resources/views/partials/profile/social.blade.php
@@ -1,48 +1,48 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-12">                      
+        <div class="col-md-12">
             @isset($persona->ficha_beneficiario->dato_social->isapre)
-            <div class="col-md-3">  
+            <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Isapre</span><br> {{ $persona->ficha_beneficiario->dato_social->isapre->organizacion }}</p>
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->ficha_beneficiario->dato_social->fonasa)
             <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Tramo Fonasa</span><br> {{ $persona->ficha_beneficiario->dato_social->fonasa->tramo }}</p>
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->ficha_beneficiario->dato_social->prevision)
             <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Previsión</span><br> {{ $persona->ficha_beneficiario->dato_social->prevision->nombre }}</p>
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->ficha_beneficiario->dato_social->sistema_proteccion)
             <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Sistema de Protección</span><br> {{ $persona->ficha_beneficiario->dato_social->sistema_proteccion->nombre }}</p>
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->educacion)
             <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Nivel Educacional</span><br> {{ $persona->educacion->nombre }}</p>
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->ocupacion)
             <div class="col-md-3">
                 <p class="capitalize"><span class="tit">Ocupación</span><br> {{ $persona->ocupacion->nombre }}</p>
             </div>
             @endisset
-        
-        
+
+
             @if(isset($persona->ficha_beneficiario->dato_social->beneficios) && count($persona->ficha_beneficiario->dato_social->beneficios) > 0)
             <div class="col-md-3">
                 <p class="tit">Beneficios</p>
@@ -51,8 +51,8 @@
                 @endforeach
             </div>
             @endif
-        
-        
+
+
             @if(isset($persona->ficha_beneficiario->dato_social->organizaciones_sociales) && count($persona->ficha_beneficiario->dato_social->organizaciones_sociales) > 0)
             <div class="col-md-3">
                 <p class="tit">Organizaciones Sociales</p>
@@ -61,7 +61,7 @@
                 @endforeach
             </div>
             @endif
-        
+
             @isset($persona->registro_social_hogar)
             <div class="col-md-3">
                 @if($persona->registro_social_hogar->en_tramite === 1)
@@ -73,8 +73,8 @@
                 @endif
             </div>
             @endisset
-        
-        
+
+
             @isset($persona->credencial_discapacidad)
             <div class="col-md-3">
                 @if($persona->credencial_discapacidad->en_tramite === 1)
@@ -85,7 +85,7 @@
                     @endisset
                 @endif
             </div>
-            @endisset            
+            @endisset
         </div>
     </div>
 </div>
--- resources/views/partials/save/social/organizaciones-sociales.blade.php
+++ resources/views/partials/save/social/organizaciones-sociales.blade.php
@@ -3,7 +3,11 @@
     <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 controls'>
         <select style="width:100%;" name='organizaciones_sociales[]' class='form-control capitalize select-tag' multiple='multiple' data-placeholder='Seleccione o agregue organizaciones...'>
             @foreach($organizaciones_sociales as $organizacion_social)
-                <option value="{{$organizacion_social->id}}">{{$organizacion_social->nombre}}</option>
+                <option value="{{$organizacion_social->id}}"
+                    @if(isset($persona) && $persona->ficha_beneficiario->dato_social->organizaciones_sociales !=null && count($persona->ficha_beneficiario->dato_social->organizaciones_sociales) > 0 && count($persona->ficha_beneficiario->dato_social->organizaciones_sociales->where('id', $organizacion_social->id)) > 0)
+                        selected
+                    @endif
+                >{{$organizacion_social->nombre}}</option>
             @endforeach
         </select>
     </div>
