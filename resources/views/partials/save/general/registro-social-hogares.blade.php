b9bd6594406afa79c69e2e5f151448fc2180a3b8
Jose Valdebenito <pvpj017@gmail.com>
2017-07-06 04:41:43 +0200

    Modified: getting information about Registro Social de Hogares on Beneficiario register

--- resources/views/partials/save/general/registro-social-hogares.blade.php
+++ resources/views/partials/save/general/registro-social-hogares.blade.php
@@ -9,9 +9,21 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class='form-group'>
                 <select style="width:100%;" name='registro_social_hogares' class='form-control select-tag' required id="registro_social_hogares">
-                   <option value='0' {{ old('registro_social_hogares') == "0" ? "selected" : ""}}>No</option>
-                   <option value='2' {{ old('registro_social_hogares') == "2" ? "selected" : ""}}>En trámite</option>
-                   <option value='1' {{ old('registro_social_hogares') == "1" ? "selected" : ""}}>Si</option>
+                   <option value="0"
+                        @if((old('registro_social_hogares') == "0") || (old('registro_social_hogares') == null && isset($persona) && $persona->registro_social_hogar == null))
+                            selected
+                        @endif
+                   >No</option>
+                   <option value="2"
+                       @if((old('registro_social_hogares') == "2") || (old('registro_social_hogares') == null && isset($persona) && $persona->registro_social_hogar != null && $persona->registro_social_hogar->en_tramite == true))
+                           selected
+                       @endif
+                   >En trámite</option>
+                   <option value="1"
+                       @if((old('registro_social_hogares') == "1") || (old('registro_social_hogares') == null && isset($persona) && $persona->registro_social_hogar != null && $persona->registro_social_hogar->en_tramite == false))
+                           selected
+                       @endif
+                   >Si</option>
                 </select>
             </div>
         </div>
@@ -19,7 +31,16 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class='form-group'>
                 <div class="input-group">
-                    <input name='registro_social_porcentaje' class='form-control' placeholder='Porcentaje' type='number' min="0" max="100" id="registro_social_porcentaje" disabled>
+                    <input name='registro_social_porcentaje'
+                    value=
+                        @if(old('registro_social_porcentaje') != null)
+                            "{{ old('registro_social_porcentaje') }}"
+                        @elseif(isset($persona) && $persona->registro_social_hogar != null)
+                            "{{ $persona->registro_social_hogar->porcentaje }}"
+                        @else
+                            ""
+                        @endif
+                    class='form-control' placeholder='Porcentaje' type='number' min="0" max="100" id="registro_social_porcentaje" disabled>
                     <span class='input-group-addon'>%</span>
                 </div>
                 <div class="help-block with-errors">
