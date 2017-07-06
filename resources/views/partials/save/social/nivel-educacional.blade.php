367f1342437bce838c94b0d401f4ac991cb0ed5e
Jose Valdebenito <pvpj017@gmail.com>
2017-07-07 00:03:06 +0200

    Modified: Educacion from partials save.social

--- resources/views/partials/save/social/nivel-educacional.blade.php
+++ resources/views/partials/save/social/nivel-educacional.blade.php
@@ -3,7 +3,11 @@
     <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
         <select style="width:100%;" name='educacion' class='form-control capitalize select-tag' id='inputSelect'>
             @foreach($niveles_educacion as $nivel_educacion)
-                <option value="{{$nivel_educacion->id}}">{{$nivel_educacion->nombre}}</option>
+                <option
+                    @if((@old('educacion') == $nivel_educacion->id) || (@old('educacion') == null && isset($persona) && $nivel_educacion->id == $persona->educacion->id))
+                        selected
+                    @endif
+                value="{{$nivel_educacion->id}}">{{$nivel_educacion->nombre}}</option>
             @endforeach
         </select>
     </div>
