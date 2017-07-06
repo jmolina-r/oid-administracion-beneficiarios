d429e4ac27da9df6dcf9fadf71a5cb9c68e51864
Jose Valdebenito <pvpj017@gmail.com>
2017-07-07 00:14:41 +0200

    Modified: ObservacionGeneral from partials save.social

--- resources/views/partials/save/social/observacion-general.blade.php
+++ resources/views/partials/save/social/observacion-general.blade.php
@@ -2,7 +2,12 @@
     <div class='box'>
         <label class='control-label' for='inputText'>Observación General</label>
         <div class='box-content'>
-            <textarea name="observacion_general" class='char-counter form-control' data-char-allowed='200' data-char-warning='10' placeholder='Ingrese una observación general...' rows='3' style='margin-bottom:10px;' value="{{ old('observacion_general') }}"></textarea>
+            <textarea name="observacion_general" class='char-counter form-control' data-char-allowed='200' data-char-warning='10' placeholder='Ingrese una observación general...' rows='3' style='margin-bottom:10px;'
+                @if(old('observacion_general'))
+                    >{{ old('observacion_general') }}</textarea>
+                @elseif(isset($persona))
+                    >{{ $persona->ficha_beneficiario->dato_social->observacion }}</textarea>
+                @endif
         </div>
     </div>
 </div>
