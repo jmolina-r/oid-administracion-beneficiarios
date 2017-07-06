ad6d976a473368d19f38edee5f37c25401a07c5c
Jose Valdebenito <pvpj017@gmail.com>
2017-07-06 23:49:51 +0200

    Modified: Estado civil from partials save.general

--- resources/views/partials/save/general/nacionalidad.blade.php
+++ resources/views/partials/save/general/nacionalidad.blade.php
@@ -3,9 +3,7 @@
     <select style="width:100%;" name='id_pais' class='form-control select-tag' id='inputSelect'>
         @foreach($paises as $pais)
             <option
-                @if(@old('id_pais') == $pais->id)
-                    selected
-                @elseif(@old('id_pais') == null && isset($persona) && $pais->id == $persona->pais->id)
+                @if((@old('id_pais') == $pais->id) || (@old('id_pais') == null && isset($persona) && $pais->id == $persona->pais->id))
                     selected
                 @endif
             value="{{$pais->id}}">{{$pais->nombre}}</option>
--- resources/views/partials/save/general/situacion-civil.blade.php
+++ resources/views/partials/save/general/situacion-civil.blade.php
@@ -2,7 +2,11 @@
     <label class='control-label' for='inputSelect'>Situación Civil</label>
     <select style="width:100%;" name='estado_civil' class='form-control capitalize select-tag' id='inputSelect'>
         @foreach($estados_civiles as $estado_civil)
-            <option value="{{$estado_civil->id}}">{{$estado_civil->nombre}}</option>
+            <option
+                @if((@old('estado_civil') == $estado_civil->id) || (@old('estado_civil') == null && isset($persona) && $estado_civil->id == $persona->estado_civil->id))
+                    selected
+                @endif
+            value="{{$estado_civil->id}}">{{$estado_civil->nombre}}</option>
         @endforeach
     </select>
 </div>
