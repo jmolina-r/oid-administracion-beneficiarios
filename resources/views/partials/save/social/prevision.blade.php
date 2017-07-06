b73c43873bd9414bedadde080bf3197866a8cbdd
Jose Valdebenito <pvpj017@gmail.com>
2017-07-06 23:40:18 +0200

    Fixed: partial prevision on save

--- resources/views/partials/save/social/prevision.blade.php
+++ resources/views/partials/save/social/prevision.blade.php
@@ -3,14 +3,14 @@
 
     <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 form-group'>
         <select style="width:100%;" name='prevision' class='form-control capitalize select-tag' id='inputSelect'>
-            @if(@old('prevision') == null && isset($persona) == null) 
+            @if(@old('prevision') == null && isset($persona) == null)
             	<option selected value=''>No tiene</option>
             @endif
             @foreach($previsiones as $prevision)
-                <option 
+                <option
                 @if(@old('prevision') == $prevision->id)
-                	selected 
-                @elseif(@old('prevision') == null && isset($persona) && $persona->ficha_beneficiario->dato_social->prevision->id == $prevision->id)
+                	selected
+                @elseif(@old('prevision') == null && isset($persona) && $persona->ficha_beneficiario->dato_social->prevision != null && $persona->ficha_beneficiario->dato_social->prevision->id == $prevision->id)
                 	selected
                 @endif
                 value="{{$prevision->id}}">{{$prevision->nombre}}</option>
