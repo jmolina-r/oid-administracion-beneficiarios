4cb85f17e00140d05e6148e7b827b58b092ade2e
Jose Valdebenito <pvpj017@gmail.com>
2017-07-07 00:00:29 +0200

    Modified: Beneficios from partials save.social

--- resources/views/partials/save/social/beneficios.blade.php
+++ resources/views/partials/save/social/beneficios.blade.php
@@ -2,8 +2,13 @@
     <label class='control-label' for='inputText'>Beneficios</label>
     <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
         <select style="width:100%;" name="beneficios[]" class='form-control select-tag' data-placeholder='Selecciona los beneficios asociados...' multiple='multiple'>
+
             @foreach($beneficios as $beneficio)
-                <option value="{{$beneficio->id}}">{{$beneficio->nombre}}</option>
+                <option value="{{$beneficio->id}}"
+                    @if(isset($persona) && $persona->ficha_beneficiario->dato_social->beneficios !=null && count($persona->ficha_beneficiario->dato_social->beneficios) > 0 && count($persona->ficha_beneficiario->dato_social->beneficios->where('id', $beneficio->id)) > 0)
+                        selected
+                    @endif
+                >{{$beneficio->nombre}}</option>
             @endforeach
         </select>
     </div>
