d3c9de2b5e4432d037f54dae08a42bb3d3267e93
Jose Valdebenito <pvpj017@gmail.com>
2017-07-07 17:06:50 +0200

    Modified: Porcentaje de discapacidad save.tipo-discapacidad paertial

--- resources/views/partials/save/discapacidad/tipo-discapacidad.blade.php
+++ resources/views/partials/save/discapacidad/tipo-discapacidad.blade.php
@@ -9,7 +9,24 @@
                 <span class='capitalize input-group-addon'>
                     {{$tipo_discapacidad->nombre}}
                 </span>
-                <input name="tipo_discapacidad[{{$tipo_discapacidad->id}}]" type="number" class="form-control bfh-number input-lg text-right" min="0" max="100" data-wrap="true" value="0">
+                <input name="tipo_discapacidad[{{$tipo_discapacidad->id}}]" type="number" class="form-control bfh-number input-lg text-right" min="0" max="100" data-wrap="true"
+                    @if(old('tipo_discapacidad') != null)
+                        value="{{ old('tipo_discapacidad') }}"
+                    @elseif(isset($persona) && $persona->ficha_beneficiario->ficha_discapacidad->porcentaje_discapacidades != null && count($persona->ficha_beneficiario->ficha_discapacidad->porcentaje_discapacidades) > 0)
+                        @php ($end = count($persona->ficha_beneficiario->ficha_discapacidad->porcentaje_discapacidades))
+                        @foreach ($persona->ficha_beneficiario->ficha_discapacidad->porcentaje_discapacidades as $key => $discapacidad)
+                            @if($discapacidad->tipo_discapacidad->id == $tipo_discapacidad->id)
+                                value="{{ $discapacidad->porcentaje }}"
+                                @break
+                            @else
+                                @if(0 == --$end)
+                                    value="0"
+                                @endif
+                            @endif
+                        @endforeach
+
+                    @endif
+                >
                 <span class="input-group-addon">%</span>
             </div>
         </div>
