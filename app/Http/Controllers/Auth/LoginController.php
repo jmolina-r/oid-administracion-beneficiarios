13cb7fb83efcedf477a10ef0e986c6949b175832
Jose Valdebenito <pvpj017@gmail.com>
2017-08-09 00:52:44 +0200

    Modified: User status setup

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,4 +41,17 @@ class LoginController extends Controller
     {
         return 'username';
     }
+
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(\Illuminate\Http\Request $request)
+    {
+        //return $request->only($this->username(), 'password');
+        return ['email' => $request->{$this->username()}, 'password' => $request->password, 'status' => 1];
+    }
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,6 +21,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('123456'),
+        'status' => 1,
         'remember_token' => str_random(10),
     ];
 });
