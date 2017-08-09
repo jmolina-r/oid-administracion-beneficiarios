f9dbae1523974837a667ec30d5ccf605d0eeac10
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-08-09 00:12:44 -0400

    Modified: Disenio buscador de usuario

--- resources/views/auth/find.blade.php
+++ resources/views/auth/find.blade.php
@@ -13,6 +13,7 @@ Lista de Usuarios - OID
 
 @section('styles')
 <link href='{{ asset('/assets/images/meta_icons/apple-touch-icon-precomposed.png') }}' rel='apple-touch-icon-precomposed'>
+<link href="assets/stylesheets/plugins/datatables/datatables.css" rel="stylesheet" type="text/css" media="all" />
 <link href="{{ asset('/css/custom.css') }}" rel="stylesheet" type="text/css" media="all" />
 @endsection
 
@@ -45,8 +46,7 @@ No importa que vayan antes del body, en el master layout se estan insertando alf
 
 <!-- / END - page related files and scripts [optional] -->
 <!-- / START - Validaciones-->
-<script src="{{ asset('/assets/javascripts/plugins/validate/jquery.validate.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/assets/javascripts/plugins/validate/additional-methods.js') }}" type="text/javascript"></script>
+<script src="assets/javascripts/plugins/datatables/datatables.min.js" type="text/javascript"></script>
 <!-- / END - validaciones-->
 <!-- / START AJAX para busqueda -->
 <script src="{{ asset('/js/beneficiario/BuscadorBeneficiario.js') }}" type="text/javascript"></script>
@@ -83,15 +83,79 @@ No importa que vayan antes del body, en el master layout se estan insertando alf
                                 <div class='box'>
                                     <div class='box-content box-padding'>
                                         <div class="container">
-                                            <div class='col-md-12 form-group'>
-                                                <label class='control-label' for='inputBuscador'>Ingrese beneficiario</label>
-                                                <div class='controls'>
-                                                    <input class='form-control' id='inputBuscador' placeholder='Ingrese nombre, apellido o rut' type='text' maxlength="200">
+                                            <div class='row'>
+                                                <div class='col-sm-12'>
+                                                    <div class='box bordered-box orange-border' style='margin-bottom:0;'>
+                                                        <div class='box-header orange-background'>
+                                                            <div class='title'>Usuario en Sistema</div>
+                                                            <div class='actions'>
+                                                                <a class="btn box-remove btn-xs btn-link" href="#"><i class='fa fa-times'></i>
+                                                                </a>
+                                                                <a class="btn box-collapse btn-xs btn-link" href="#"><i></i>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                        <div class='box-content box-no-padding'>
+                                                            <div class='responsive-table'>
+                                                                <div class='scrollable-area'>
+                                                                    <table class='data-table table table-bordered table-striped' style='margin-bottom:0;'>
+                                                                        <thead>
+                                                                            <tr>
+                                                                                <th>
+                                                                                    Name
+                                                                                </th>
+                                                                                <th>
+                                                                                    E-mail
+                                                                                </th>
+                                                                                <th>
+                                                                                    Status
+                                                                                </th>
+                                                                                <th></th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            <tr>
+                                                                                <td>José Valdebenito Pertierra</td>
+                                                                                <td>jose@elpro.fr</td>
+                                                                                <td>
+                                                                                    <span class='label label-success'>Pulento</span>
+                                                                                </td>
+                                                                                <td>
+                                                                                    <div class='text-right'>
+                                                                                        <a class='btn btn-success btn-xs' href='#'>
+                                                                                            <i class='fa fa-check'></i>
+                                                                                        </a>
+                                                                                        <a class='btn btn-danger btn-xs' href='#'>
+                                                                                            <i class='fa fa-times'></i>
+                                                                                        </a>
+                                                                                    </div>
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td>Arnoldo</td>
+                                                                                <td>ano@asdsa.com</td>
+                                                                                <td>
+                                                                                    <span class='label label-success'>Gay</span>
+                                                                                </td>
+                                                                                <td>
+                                                                                    <div class='text-right'>
+                                                                                        <a class='btn btn-success btn-xs' href='#'>
+                                                                                            <i class='fa fa-check'></i>
+                                                                                        </a>
+                                                                                        <a class='btn btn-danger btn-xs' href='#'>
+                                                                                            <i class='fa fa-times'></i>
+                                                                                        </a>
+                                                                                    </div>
+                                                                                </td>
+                                                                            </tr>
+                                                                        </tbody>
+                                                                    </table>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="listaBeneficiario">   
-                                            </div>
-
 
                                         </div>
                                     </div>
