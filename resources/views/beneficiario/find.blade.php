026f8c05ba2e7d2f829ee7802decc71aefb7f0e0
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-08-10 12:38:47 -0400

    Modified: Buscador beneficiario

--- resources/views/beneficiario/find.blade.php
+++ resources/views/beneficiario/find.blade.php
@@ -25,87 +25,119 @@ class='contrast-red'
 @endsection
 
 <!-- Inyeccion de scripts
-     No importa que vayan antes del body, en el master layout se estan insertando alfinal.
-   -->
-   @section('scripts')
-   <!-- / jquery [required] -->
-    <script src="{{ asset('/assets/javascripts/jquery/jquery.min.js') }}" type="text/javascript"></script>
-    <!-- / jquery mobile (for touch events) -->
-    <script src="{{ asset('/assets/javascripts/jquery/jquery.mobile.custom.min.js') }}" type="text/javascript"></script>
-    <!-- / jquery ui -->
-    <script src="{{ asset('/assets/javascripts/jquery/jquery-ui.min.js') }}" type="text/javascript"></script>
-    <!-- / jQuery UI Touch Punch -->
-    <script src="{{ asset('/assets/javascripts/jquery/jquery.ui.touch-punch.min.js') }}" type="text/javascript"></script>
-    <!-- / bootstrap [required] -->
-    <script src="{{ asset('/assets/javascripts/bootstrap/bootstrap.js') }}" type="text/javascript"></script>
-    <!-- / modernizr -->
-    <script src="{{ asset('/assets/javascripts/plugins/modernizr/modernizr.min.js') }}" type="text/javascript"></script>
-    <!-- / retina -->
-    <script src="{{ asset('/assets/javascripts/plugins/retina/retina.js') }}" type="text/javascript"></script>
-    <!-- / theme file [required] -->
-    <script src="{{ asset('/assets/javascripts/theme.js') }}" type="text/javascript"></script>
-    <!-- / START - page related files and scripts [optional] -->
+No importa que vayan antes del body, en el master layout se estan insertando alfinal.
+-->
+@section('scripts')
+<!-- / jquery [required] -->
+<script src="{{ asset('/assets/javascripts/jquery/jquery.min.js') }}" type="text/javascript"></script>
+<!-- / jquery mobile (for touch events) -->
+<script src="{{ asset('/assets/javascripts/jquery/jquery.mobile.custom.min.js') }}" type="text/javascript"></script>
+<!-- / jquery ui -->
+<script src="{{ asset('/assets/javascripts/jquery/jquery-ui.min.js') }}" type="text/javascript"></script>
+<!-- / jQuery UI Touch Punch -->
+<script src="{{ asset('/assets/javascripts/jquery/jquery.ui.touch-punch.min.js') }}" type="text/javascript"></script>
+<!-- / bootstrap [required] -->
+<script src="{{ asset('/assets/javascripts/bootstrap/bootstrap.js') }}" type="text/javascript"></script>
+<!-- / modernizr -->
+<script src="{{ asset('/assets/javascripts/plugins/modernizr/modernizr.min.js') }}" type="text/javascript"></script>
+<!-- / retina -->
+<script src="{{ asset('/assets/javascripts/plugins/retina/retina.js') }}" type="text/javascript"></script>
+<!-- / theme file [required] -->
+<script src="{{ asset('/assets/javascripts/theme.js') }}" type="text/javascript"></script>
+<!-- / START - page related files and scripts [optional] -->
 
-    <!-- / END - page related files and scripts [optional] -->
-   <!-- / START - Validaciones-->
-   <script src="{{ asset('/assets/javascripts/plugins/validate/jquery.validate.min.js') }}" type="text/javascript"></script>
-   <script src="{{ asset('/assets/javascripts/plugins/validate/additional-methods.js') }}" type="text/javascript"></script>
-   <script src="{{ asset('/assets/javascripts/plugins/1000hz-bootstrap-validator/validator.min.js') }}"></script>
-   <script src="{{ asset('/js/InputValidation.js') }}" type="text/javascript"></script>
-  <!-- / END - validaciones-->
-   <!-- / START AJAX para busqueda -->
-   <script src="{{ asset('/js/beneficiario/BuscadorBeneficiario.js') }}" type="text/javascript"></script>
-   <!-- / END  -->
-  @endsection
+<!-- / END - page related files and scripts [optional] -->
+<!-- / START - Validaciones-->
+<script src="{{ asset('/assets/javascripts/plugins/validate/jquery.validate.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/assets/javascripts/plugins/validate/additional-methods.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/assets/javascripts/plugins/1000hz-bootstrap-validator/validator.min.js') }}"></script>
+<script src="{{ asset('/js/InputValidation.js') }}" type="text/javascript"></script>
+<!-- / END - validaciones-->
+<!-- / START AJAX para busqueda -->
+<script src="{{ asset('/js/beneficiario/BuscadorBeneficiario.js') }}" type="text/javascript"></script>
+<!-- / END  -->
+@endsection
 
-  <!-- Contenido del body -->
-  @section('content')
-  @include('partials.header')
-  <div id='wrapper'>
+<!-- Contenido del body -->
+@section('content')
+@include('partials.header')
+<div id='wrapper'>
     <div id='main-nav-bg'></div>
     @include('partials.nav')
     <section id='content'>
-      <div class='container'>
-        <div class='row' id='content-wrapper'>
-          <div class='col-xs-12'>
-            <div class='row'>
-              <div class='col-sm-12'>
-                <div class='page-header'>
-                  <h1 class='pull-left'>
-                    <i class='fa fa-pencil-square-o'></i>
-                    <span>Buscador de Beneficiario</span>
-                  </h1>
-                  <div class='pull-right'>
-                    <ul class='breadcrumb'>
-                      <li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class='row'>
-              <div class='col-sm-12'>
-                <div class='box'>
-                  <div class='box-content box-padding'>
-                    <div class="container">
-                      <div class='col-md-12 form-group'>
-                       <label class='control-label' for='inputBuscador'>Ingrese beneficiario</label>
-                       <div class='controls'>
-                        <input class='form-control' id='inputBuscador' placeholder='Ingrese nombre, apellido o rut' type='text' maxlength="200">
-                       </div>
-                      </div>
-                      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="listaBeneficiario">   
-                      </div>
+        <div class='container'>
+            <div class='row' id='content-wrapper'>
+                <div class='col-xs-12'>
+                    <div class='row'>
+                        <div class='col-sm-12'>
+                            <div class='page-header'>
+                                <h1 class='pull-left'>
+                                    <i class='fa fa-pencil-square-o'></i>
+                                    <span>Buscador de Beneficiario</span>
+                                </h1>
+                                <div class='pull-right'>
+                                    <ul class='breadcrumb'>
+                                        <li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='row'>
+                            <div class='col-sm-12'>
+                                <div class='box'>
+                                    <div class='box-content box-padding'>
+                                        <div class="container">
+                                            <div class='col-md-12 form-group'>
+                                                <label class='control-label' for='inputBuscador'>Ingrese beneficiario</label>
+                                                <div class='controls'>
+                                                    <input class='form-control' id='inputBuscador' placeholder='Ingrese nombre, apellido o rut' type='text' maxlength="200">
+                                                </div>
+                                            </div>
+                                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                                <div class='box bordered-box green-border' style='margin-bottom:0;'>
+                                                    <div class='box-content box-no-padding'>
+                                                        <div class='responsive-table'>
+                                                            <table class='table table-bordered table-hover table-striped' style='margin-bottom:0;'>
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th>
+                                                                            Nombre
+                                                                        </th>
+                                                                        <th>
+                                                                            Rut
+                                                                        </th>
+                                                                        <th>
+                                                                            Género
+                                                                        </th>
+                                                                        <th>
+                                                                            Registrado
+                                                                        </th>
+                                                                        <th></th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody id="listaBeneficiario">
+                                                                    <tr>
+                                                                        <td>
+                                                                            No hay datos para mostrar.
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
+            @include('partials.footer')
         </div>
-      </div>
-      @include('partials.footer')
-    </div>
-  </section>
+    </section>
 </div>
 @endsection
